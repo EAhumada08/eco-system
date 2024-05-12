f15df7404bd2233100b4c0f1c7e64080b47c321b
EAhumada08 <emaahmc08@gmail.com>
2024-05-11 19:31:13 -0600

    addLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import { NavLink } from 'react-router-dom'
+
 export default function Header () {
   return (
     <header className=' bg-[#00BF63] h-fit p-2 flex shadow-lg shadow-cyan-500/50 '>
       <div className=' w-20'>
-        <img src='../public/imgs/logoES.png' alt='' />
+        <NavLink to='/'> <img src='../public/imgs/logoES.png' alt='' /> </NavLink>
       </div>
     </header>
   )
